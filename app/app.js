4c6d69d60b2913daced02fb282e5430154bd0b67
Pavlo <p.kalashnikov@epages.com>
2022-02-12 14:03:54 +0100

    New: Tabs added

--- app/app.js
+++ app/app.js
@@ -1,5 +1,22 @@
 let sportUiApp = angular.module('sport-ui-app', ['ngRoute', 'ngMaterial']);
 
+sportUiApp.controller('SportUiMainController', ['$scope', '$http', function ($scope, $http) {
+
+    $http.get('data/balance.json')
+        .then(function (response) {
+            $scope.balance = response.data;
+        }, function () {
+            console.log("Get balance failed.");
+        });
+
+    $http.get('data/activities.json')
+        .then(function (response) {
+            $scope.activities = response.data;
+        }, function () {
+            console.log("Get activities failed.");
+        });
+}]);
+
 sportUiApp.run(function () {
 
 });
