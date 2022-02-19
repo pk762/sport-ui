98e93dae85e2049b435c39d2dad1ac78327643cf
Pavlo <p.kalashnikov@epages.com>
2022-02-19 15:00:01 +0100

    Update: Balance update

--- app/app.js
+++ app/app.js
@@ -2,19 +2,59 @@ let sportUiApp = angular.module('sport-ui-app', ['ngRoute', 'ngMaterial']);
 
 sportUiApp.controller('SportUiMainController', ['$scope', '$http', function ($scope, $http) {
 
-    $http.get('data/balance.json')
-        .then(function (response) {
-            $scope.balance = response.data;
-        }, function () {
-            console.log("Get balance failed.");
-        });
+    $scope.newActivity = {
+        time: "30min",
+        date: new Date(),
+        work: "Basic gymnastic"
+    };
 
-    $http.get('data/activities.json')
-        .then(function (response) {
-            $scope.activities = response.data;
-        }, function () {
-            console.log("Get activities failed.");
-        });
+    const dateFormat = {weekday: "short", month: "short", year: "numeric", day: "2-digit"};
+
+
+    $scope.addActivity = function () {
+
+        const data = {
+            activityName: $scope.newActivity.work,
+            time: $scope.newActivity.time
+        };
+
+        $http.post(window.__env.apiUrl + "/activities", data)
+            .then(function (response) {
+                console.log(response.data)
+            }, function (response) {
+                console.log(response.data)
+            });
+
+        $scope.onBalanceSelect();
+
+        console.log($scope.newActivity.time + ":" + $scope.newActivity.date.toLocaleDateString(
+            "de-DE", {weekday: "short", month: "short", year: "numeric", day: "2-digit"}
+        ));
+    };
+
+    $scope.onActivitiesSelect = function () {
+        $http.get(window.__env.apiUrl + "/activities")
+            .then(function (response) {
+                $scope.activities = response.data.map(item => ({
+                    time: item.duration,
+                    date: new Date(item.createdAt).toLocaleDateString("de-DE", dateFormat),
+                    earn: "0,50 €",
+                    work: item.activity
+                }))
+            }, function (response) {
+                console.log("Get activities failed." + response.statusText);
+            })
+    };
+
+    $scope.onBalanceSelect = function () {
+        $http.get(window.__env.apiUrl + "/balance")
+            .then(function (response) {
+                $scope.balance = response.data.value;
+                console.log(response.data)
+            }, function (response) {
+                console.log("Get balance failed. " + response.statusText)
+            })
+    };
 }]);
 
 sportUiApp.run(function () {
---
+++ app/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+    window.__env = window.__env || {};
+    window.__env.apiUrl = 'https://sport-activity.herokuapp.com';
+    window.__env.baseUrl = '/';
+    window.__env.enableDebug = true;
+}(this));
